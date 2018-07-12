c43d5443e16b46d0166ae26fae1870e2dd894b0f
Sreeni Jilla <37336941+dssreeni@users.noreply.github.com>
2018-07-12 18:16:28 +0530

    Uploading basic data types and containers code
    Uploading basic data types and containers code

---
+++ Python/2.list.py
@@ -0,0 +1,28 @@
+#Basic List containers
+
+#For lists use [] brackets
+list1 = [10,20,30]
+print(type(list1))
+
+list2=['abc', 10, True]
+print(type(list2))
+
+#access list elements by slicing
+print(list1[0])
+print(list1[0:2])
+print(list1[2:3])
+
+list1[2] = 100
+print(list1)
+
+list1.append(10) 
+list1.extend(list2)
+
+#extend expects a values/collection of values
+list1.extend(10)
+
+#pop is for removing by index
+list1.pop(1)
+
+list3 = [list1, 60, list2]
+len(list3)
